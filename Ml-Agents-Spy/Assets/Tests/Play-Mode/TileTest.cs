9cee9daf24ba2139362af38caab1925dee27fb52
HPRIOR <hprior02@dcs.bbk.ac.uk>
2020-07-09 17:51:51 +0100

    commit before adding reference assembly

--- Ml-Agents-Spy/Assets/Tests/Play-Mode/TileTest.cs
+++ Ml-Agents-Spy/Assets/Tests/Play-Mode/TileTest.cs
@@ -6,20 +6,22 @@ using UnityEngine.TestTools;
 using UnityEngine.Tilemaps;
 using UnityEngine.WSA;
 
+
+
 namespace Tests
 {
-    // public class TileTest
-    // {
-    //     // A Test behaves as an ordinary method
-    //     [Test]
-    //     public void TileTestSimplePasses()
-    //     {
-    //         Tile t = new Tile(new Vector3(0, 0, 0), (0, 0));
-    //         Assert.AreEqual(false, t.HasEnv);
-    //         Assert.AreEqual(false, t.HasGuard);
-    //         Assert.AreEqual(false, t.HasSpy);
-    //         Assert.AreEqual(false, t.IsExit);
-    //         Assert.AreEqual(false, t.OnSpyPath);
-    //     }
-    // }
+    public class TileTest
+    {
+        // A Test behaves as an ordinary method
+        [Test]
+        public void TileTestSimplePasses()
+        {
+            Tile t = new Tile(new Vector3(0, 0, 0), (0, 0));
+            Assert.AreEqual(false, t.HasEnv);
+            Assert.AreEqual(false, t.HasGuard);
+            Assert.AreEqual(false, t.HasSpy);
+            Assert.AreEqual(false, t.IsExit);
+            Assert.AreEqual(false, t.OnSpyPath);
+        }
+    }
 }
