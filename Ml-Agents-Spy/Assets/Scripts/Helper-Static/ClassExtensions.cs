c5eb3f7b930e5314def991613eedb9b2c22a075e
HPRIOR <hprior02@dcs.bbk.ac.uk>
2020-07-28 10:08:01 +0100

    change namespaces for envsetup

--- Ml-Agents-Spy/Assets/Scripts/Helper-Static/ClassExtensions.cs
+++ Ml-Agents-Spy/Assets/Scripts/Helper-Static/ClassExtensions.cs
@@ -1,13 +1,10 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Runtime.CompilerServices;
-using UnityEngine;
+using EnvSetup;
 
 public static class ClassExtensions 
 {
-    public static List<EnvTile> CloneTileList(this List<EnvTile> inputList) =>
+    public static List<EnvTile> CloneTileList(this IEnumerable<EnvTile> inputList) =>
         inputList.Select(tile => (EnvTile)tile.Clone()).ToList();
 
     public static T MostRecentlyAdded<T>(this Queue<T> queue) => queue.ToArray().ToList().Last();
