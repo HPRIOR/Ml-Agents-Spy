280037efd14c00fe3d658ea630ed24d039738a4d
HPRIOR <hprior02@dcs.bbk.ac.uk>
2020-08-02 22:26:55 +0100

    adding tests for GetNearest GameObject extension method

--- Ml-Agents-Spy/Assets/Tests/Edit-Mode/TestGetNearestGameObject.cs
+++ Ml-Agents-Spy/Assets/Tests/Edit-Mode/TestGetNearestGameObject.cs
@@ -8,16 +8,54 @@ namespace Tests
 {
     public class TestGetNearestGameObject
     {
-        // A Test behaves as an ordinary method
-        [Test]
-        public void NewTestScriptSimplePasses()
+       
+        
+        public (GameObject, List<GameObject>) GetThisGameObjectAndNearest()
         {
             GameObject thisGameObject = new GameObject();
             thisGameObject.transform.position = new Vector3(0, 0, 0);
             
+            GameObject nearestGameObject = new GameObject();
+            nearestGameObject.transform.position = new Vector3(1f, 0,0);
+            
+            GameObject secondNearestGameObject = new GameObject();
+            secondNearestGameObject.transform.position = new Vector3(2f, 0,0);
+            
+            GameObject thirdNearestGameObject = new GameObject();
+            thirdNearestGameObject.transform.position = new Vector3(3f, 0,0);
+            
+            GameObject fourthNearestGameObject = new GameObject();
+            fourthNearestGameObject.transform.position = new Vector3(4f, 0,0);
+            
+            GameObject fifthNearestGameObject = new GameObject();
+            fifthNearestGameObject.transform.position = new Vector3(5f, 0,0);
+            
+            GameObject sixthNearestGameObject = new GameObject();
+            sixthNearestGameObject.transform.position = new Vector3(6f, 0,0);
+            
+            GameObject seventhNearestGameObject = new GameObject();
+            seventhNearestGameObject.transform.position = new Vector3(7f, 0,0);
 
+            return (thisGameObject, new List<GameObject>()
+            {
+                nearestGameObject, 
+                secondNearestGameObject, 
+                thirdNearestGameObject, 
+                fourthNearestGameObject, 
+                fifthNearestGameObject, 
+                sixthNearestGameObject, 
+                seventhNearestGameObject
+            });
         }
 
+        [Test]
+        public void TestNearestCount()
+        {
+            (GameObject thisGameObject, List<GameObject> nearestList) = GetThisGameObjectAndNearest();
+            
+            thisGameObject.GetNearest()
+        }
+        
         
     }
 }
